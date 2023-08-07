da360060758151da0c70f70da5dde5e119dd9d12
hannan <abdullhannan0311@gmail.com>
2023-08-07 12:15:46 +0500

    new

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webView.setWebViewClient(new CustomWebViewClient());
-        webView.loadUrl("https://demolike.cleverapps.io/z1");
+        webView.loadUrl("https://yahoewa.cleverapps.io/e1");
 
 
     }
